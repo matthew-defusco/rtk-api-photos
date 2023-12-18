23d80a3fd73fa197f1352244b630c5b2e1e7d4ee
Matt <matthew.defusco@gmail.com>
2023-12-18 17:18:53 -0500

    full project complete

--- src/main.js
+++ src/main.js
@@ -7,9 +7,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
